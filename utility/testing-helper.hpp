da918c7d529e73209f7160e61d61273a4cedc6aa
Ian Fang <xfang@uwf.edu>
2022-10-23 13:59:42 -0500

    improved helper functions

--- utility/testing-helper.hpp
+++ utility/testing-helper.hpp
@@ -2,33 +2,32 @@
 #define TESTING_HELPER_HPP
 
 #include <cmath>
-#include <vector>
-
-using std::vector;
+#include <iostream>
 
 bool isClose(double v1, double v2, double epsilon = 1e-4);
-
-template<class T>
-bool allPositive(const vector<T> &vec);
-
-template<class T>
-void printVector(const vector<T> &vec);
-
 bool isClose(double v1, double v2, double epsilon) {
   return std::abs(v1 - v2) < epsilon;
 }
 
-template<class T>
-bool allNotNegative(const vector<T> &vec) {
-  for (auto v: vec)
-    if (v < 0) return false;
+template<class IT>
+bool isAllNotNegative(IT start, IT end) {
+  for (IT it = start; it != end; ++it)
+    if (*it < 0) return false;
   return true;
 }
 
-template<class T>
-void printVector(const vector<T> &vec) {
-  for (auto v: vec)
-    std::cout << v << " ";
+template<class IT>
+bool isAscending(IT start, IT end) {
+  for (IT it = start; it != end - 1; ++it)
+    if (*it > *(it + 1)) return false;
+  return true;
+}
+
+template<class IT>
+void printSeq(IT start, IT end) {
+  for (IT it = start; it != end; ++it)
+    std::cout << *it << " ";
   std::cout << std::endl;
 }
+
 #endif // TESTING_HELPER_HPP
