8b198f70f5c39486022ad37769e13f34b7c021ef
Ian Fang <xfang@uwf.edu>
2023-03-01 12:43:07 -0600

    add doubly linked list

--- ch07-linked-list/doubly-ll.hpp
+++ ch07-linked-list/doubly-ll.hpp
@@ -1,42 +1,155 @@
 #ifndef DOUBLY_LL_HPP
 #define DOUBLY_LL_HPP
 
-template<class T>
-class BidirectionalNode {
-  T value;
-  BidirectionalNode<T> * next;
-  BidirectionalNode<T> * prev;
- public:
-  BidirectionalNode<T>();
-  BidirectionalNode<T>(T value): value(value) {}
-  void setValue(T value);
-  void setNext(BidirectionalNode<T> *next);
-  void setPrev(BidirectionalNode<T> *prev);
-  T getValue() const;
-  BidirectionalNode<T> *getNext() const;
-  BidirectionalNode<T> *getPrev() const;
-};
-
+// This implementation is an example of a Deque implemented as a
 // doubly-linked list
-// a head pointer, a tail pointer
-// a next pointer, a prev pointer in node class
-template <class T>
+// It has a functionalities required by a Deque with two additional methods:
+//    size check, and remove by value
+// It is not an exact Deque for that reason.
+template <typename T>
 class DoublyLinkedList {
  private:
-  BidirectionalNode<T> * head;
-  BidirectionalNode<T> * tail;
+  struct Node {
+    T data;
+    Node* next;
+    Node* prev;
+
+    Node(const T& val) : data(val), next(nullptr), prev(nullptr) {}
+  };
+
+  Node* head;
+  Node* tail;
+  size_t count;
+
  public:
-  DoublyLinkedList();
-  ~DoublyLinkedList();
-  void append(Node<T> * node);
-  void prepend(Node<T> * node);
-  void insertBefore(Node<T> * refNode, Node<T> * nodeToInsert);
-  void insertAfter(Node<T> * refNode, Node<T> * nodeToInsert);
-  void remove(Node<T> *node);
-  Node<T> *search(T value);
-  int size();
-  bool isEmpty();
-  void print();
+  DoublyLinkedList() : head(nullptr), tail(nullptr), count(0) {}
+
+  ~DoublyLinkedList() {
+    Node* curr = head;
+    while (curr) {
+      Node* temp = curr;
+      curr = curr->next;
+      delete temp;
+    }
+  }
+
+  bool empty() const { return count == 0; }
+
+  size_t size() const { return count; }
+
+  void clear() {
+    Node* curr = head;
+    while (curr) {
+      Node* temp = curr;
+      curr = curr->next;
+      delete temp;
+    }
+    head = nullptr;
+    tail = nullptr;
+    count = 0;
+  }
+
+  void push_front(const T& val) {
+    Node* new_node = new Node(val);
+    if (empty()) {
+      head = tail = new_node;
+    } else {
+      new_node->next = head;
+      head->prev = new_node;
+      head = new_node;
+    }
+    count++;
+  }
+
+  void push_back(const T& val) {
+    Node* new_node = new Node(val);
+    if (empty()) {
+      head = tail = new_node;
+    } else {
+      new_node->prev = tail;
+      tail->next = new_node;
+      tail = new_node;
+    }
+    count++;
+  }
+
+  void pop_front() {
+    if (empty()) {
+      throw std::out_of_range("DoublyLinkedList is empty");
+    }
+    Node* temp = head;
+    head = head->next;
+    if (head) {
+      head->prev = nullptr;
+    } else {
+      tail = nullptr;
+    }
+    delete temp;
+    count--;
+  }
+
+  void pop_back() {
+    if (empty()) {
+      throw std::out_of_range("DoublyLinkedList is empty");
+    }
+    Node* temp = tail;
+    tail = tail->prev;
+    if (tail) {
+      tail->next = nullptr;
+    } else {
+      head = nullptr;
+    }
+    delete temp;
+    count--;
+  }
+
+  const T& front() const {
+    if (empty()) {
+      throw std::out_of_range("DoublyLinkedList is empty");
+    }
+    return head->data;
+  }
+
+  T& front() {
+    if (empty()) {
+      throw std::out_of_range("DoublyLinkedList is empty");
+    }
+    return head->data;
+  }
+
+  const T& back() const {
+    if (empty()) {
+      throw std::out_of_range("DoublyLinkedList is empty");
+    }
+    return tail->data;
+  }
+
+  T& back() {
+    if (empty()) {
+      throw std::out_of_range("DoublyLinkedList is empty");
+    }
+    return tail->data;
+  }
+
+  void remove(const T& val) {
+    Node* curr = head;
+    while (curr) {
+      if (curr->data == val) {
+        if (curr == head) {
+          pop_front();
+        } else if (curr == tail) {
+          pop_back();
+        } else {
+          curr->prev->next = curr->next;
+          curr->next->prev = curr->prev;
+          delete curr;
+          count--;
+        }
+        return;
+      }
+      curr = curr->next;
+    }
+  }
 };
 
 #endif
