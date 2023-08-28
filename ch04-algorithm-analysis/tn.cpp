d82f8aa34e7629c6da8473a967d3437b134cc10c
Ian Fang <xfang@uwf.edu>
2023-08-27 23:40:06 -0500

    add more analysis example

--- ch04-algorithm-analysis/tn.cpp
+++ ch04-algorithm-analysis/tn.cpp
@@ -5,8 +5,8 @@ int search(int * arr, int size, int val) {
       return i;
   return -1;
 }
-// 1 + n + 1 + n + n + 1 = 3n + 3, worst O(n)
-// 1 + 1 + 1 + 1 = 4, best
+// worst case: T(n) = 1 + n + 1 + n + n + 1 = 3n + 3 = O(n)
+// Best case: T(n) 1 + 1 + 1 + 1 = 4 = O(1)
 
 
 // search linearly, continue on match
@@ -21,7 +21,29 @@ int search1(int * arr, int size, int val) {
   }
   return idx;
 }
-// Analysis
-// 1 + 1 + 1 + 4 * n + 1 = 4n + 4, worst case
-// 1 + 1 + 1 + 3 * n + 1 = 3n + 4, best case
-//O(4n + 4) = O(n)
+// worst case: T(n) = 1 + 1 + 1 + 4 * n + 1 = 4n + 4 = O(n)
+// best case: T(n) = 1 + 1 + 1 + 3 * n + 1 = 3n + 4 = O(n)
+
+// selection sort
+void sort(int * arr, int size) {
+  for (int i = 0; i < size - 1; ++i) {
+    int min = i;
+    for (int j = i + 1; j < size; ++j)
+      if (arr[j] < arr[min])
+        min = j;
+    if (min != i) {
+      int tmp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = tmp;
+    }
+  }
+}
+// T(n) = number of comparisons + number of swaps
+// number of comparisons = 1 + 2 + 3 + ... + n - 1 = n(n - 1) / 2
+// number of swaps = n - 1
+
+// worst case: no swap needed, sorting a sorted array
+// T(n) = 1 + 2 + 3 + ... + n - 1 = n(n - 1) / 2 = O(n^2)
+
+// best case: swap needed every time, sorting a reverse sorted array
+// T(n) = n(n - 1) / 2 + n - 1 = O(n^2)
