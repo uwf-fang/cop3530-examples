98cffa53d37afaabb8a87d28e5356441060d2761
Ian Fang <xfang@uwf.edu>
2024-03-09 02:01:34 -0600

    zybook quicksort and new tests

--- m09-sorting/quicksort.cpp
+++ m09-sorting/quicksort.cpp
@@ -7,13 +7,16 @@ void quicksortHelper(int *arr, int low, int high);
 int quickPartition(int *arr, int low, int high);
 
 int quickPartition(int *arr, int low, int high) {
-  int mid = (high + low) / 2;
-  int pivot = arr[mid];
+  // int mid = low + (high - low) / 2;
+  // int pivot = arr[mid];
+  int pivot = arr[low];
   while (true) {
     while (arr[low] < pivot) ++low;
     while (arr[high] > pivot) --high;
     if (low >= high) break;
     swap(arr[low], arr[high]);
+    ++low;
+    --high;
   }
   return high;
 }
@@ -25,6 +28,6 @@ void quicksort(int *arr, int size) {
 void quicksortHelper(int *arr, int low, int high) {
   if (low >= high) return;
   int part = quickPartition(arr, low, high);
-  quicksortHelper(arr, low, part - 1);
+  quicksortHelper(arr, low, part);
   quicksortHelper(arr, part + 1, high);
 }
--- m09-sorting/test-quicksort.cpp
+++ m09-sorting/test-quicksort.cpp
@@ -6,18 +6,47 @@
 
 using namespace std;
 
-int main() {
-  int size = 10;
-  int *arr = new int[size];
+// Test that the mid formulas are equivalent
+// (i + j) / 2 and i + (j - i) / 2
+void testMidFormula() {
+  for (int i = 0; i < 3; ++i)
+    for (int j = 8; j < 11; ++j) {
+      int mid1 = (i + j) / 2;
+      int mid2 = i + (j - i) / 2;
+      assert(mid1 == mid2);
+    }
+}
 
+void testNoDup() {
+  const int SIZE = 10;
+  int arr[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  // no duplicates
   for (int run = 0; run < 3; ++ run) {
-    for (int i = 0; i < size; ++i) arr[i] = i + 1;
-    shuffle(arr, arr + size, default_random_engine(0));
-    assert(!is_sorted(arr, arr + size));
-    quicksort(arr, size);
-    assert(is_sorted(arr, arr + size));
-    cout << "Run " << run << " passed!" << endl;
+    shuffle(arr, arr + SIZE, default_random_engine(0));
+    assert(!is_sorted(arr, arr + SIZE));
+    quicksort(arr, SIZE);
+    assert(is_sorted(arr, arr + SIZE));
+    cout << "Test on array with no duplicate: run " <<
+        run << " passed!" << endl;
   }
+}
+
+void testWithDup() {
+  const int SIZE = 10;
+  int arr[SIZE] = {1, 7, 3, 4, 5, 6, 7, 8, 3, 10};  // with duplicates
+  for (int run = 0; run < 3; ++ run) {
+    shuffle(arr, arr + SIZE, default_random_engine(0));
+    assert(!is_sorted(arr, arr + SIZE));
+    quicksort(arr, SIZE);
+    assert(is_sorted(arr, arr + SIZE));
+    cout << "Test on array with duplicates: run " <<
+        run << " passed!" << endl;
+  }
+}
+
+int main() {
+  // testMidFormula();
+  testNoDup();
+  testWithDup();
 
   return EXIT_SUCCESS;
 }
